396437cec974a409ab7a6dac78dc979664b7615d
cyh789 <drsggg@naver.com>
2022-05-21 14:41:11 +0900

    Feat: "Add 숫자야구게임 - 문자열계산기"

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -13,11 +13,4 @@ public class Calculator {
     int divide(int a, int b){
         return a/b;
     }
-    public static void main(String[] args){
-        Calculator cal= new Calculator();
-        System.out.println(cal.add(3,4));
-        System.out.println(cal.subtract(5,4));
-        System.out.println(cal.multiply(2,6));
-        System.out.println(cal.divide(8,4));
-    }
 }

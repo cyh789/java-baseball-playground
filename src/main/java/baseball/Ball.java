7dffc3b6e79248037f68891d25796b084311d68c
cyh789 <drsggg@naver.com>
2022-05-23 00:54:27 +0900

    Feat: "Add 숫자야구게임 - 중간구현3. 숫자 야구 게임 구현"

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -3,11 +3,18 @@ package baseball;
 public class Ball {
 
     private final int position;
-    private final int ballNo;
+
+    //* 클래스분리 연습 *
+    //객체지향 생활체조 -> 3.모든 원시 값과 문자열을 포장한다.
+    //포장하게 되면 유효성체크를 할 수 있게 되므로, 값을 보장할 수 있게 된다.
+    //유효성체크를 유틸 클래스를 만들어서 처리하지 말고, 원시값이나 문자열을 포장해서 처리하면 뭔가 로직을 추가 할 수 있다(클래스 분리)
+//    private final int ballNo;
+    private final BallNumber ballNo;
 
     public Ball(int position, int ballNo) {
         this.position = position;
-        this.ballNo = ballNo;
+//        this.ballNo = ballNo;
+        this.ballNo = new BallNumber(ballNo);
     }
 
     public BallStatus play(Ball ball) {
@@ -24,7 +31,7 @@ public class Ball {
         return this.position == position;
     }
 
-    private boolean matchBallNo(int ballNo) {
+    private boolean matchBallNo(BallNumber ballNo) {
         return this.ballNo == ballNo;
     }
 
--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -3,11 +3,11 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Balls {
-    private final List<Ball> answersBallList;
+public class Balls implements Playable {
+    private final List<Ball> answersBallNumbers;
 
     public Balls(List<Integer> answers) {
-        this.answersBallList = mapBall(answers);
+        this.answersBallNumbers = mapBall(answers);
     }
 
     //리팩토링 : 인스턴스 변수에 의존하는 곳이 없으므로 static을 붙여서 클래스변수로 사용
@@ -19,8 +19,10 @@ public class Balls {
         return balls;
     }
 
-    public BallStatus play(Ball userBall) {
-        return answersBallList.stream()
+    //1.
+    //private으로 변경 가능
+    BallStatus play(Ball userBall) {
+        return answersBallNumbers.stream()
                 .map(answers -> answers.play(userBall))
                 .filter(BallStatus::isNotNothing)
 //                .filter(status -> status.isNotNothing())
@@ -29,14 +31,22 @@ public class Balls {
                 .orElse(BallStatus.NOTHING);
     }
 
-    public PlayResult play(List<Integer> userBallList) {
-        Balls userBalls = new Balls(userBallList);
+    //1. 핵심이 되는 기능은?
+    //핵심이 되는 기능들 : play
+    //핵심이 되는 기능들 중 public으로 노출이 되는 기능 : play
+    //2. 핵심기능을 interface화 한다면?
+    //위의 경우에는 interface로 만들 수 있음. 다만 필요한 시점에 뽑아내는것이 좋다(interface가 남발될 수 있음)
+    //필요한 시점 : 새로운 방식의 play, 새로운 구현체 Balls(새로운 방식을 가진 Balls) 가 생기는 시점
+    //3. 변수명을 userBallList로 쓰면?
+    //List나 Set 같은 자료구조의 이름을 변수에 적는것은 적절하지 않다. 자료구조가 바뀌게 될 경우, 변수명을 바꿔줘야 되므로.
+    public PlayResult play(List<Integer> userBallNumbers) {
+        Balls userBalls = new Balls(userBallNumbers);
         PlayResult result = new PlayResult();
-        for (Ball ballList : answersBallList) {
-            //answersBallList = ballList : 원래 있던 값(정답)
-            //userBallList : 유저가 넣어준 값
-            //userBallList로 만든 Balls 객체 : userBalls
-            BallStatus status = userBalls.play(ballList);
+        for (Ball BallNumbers : answersBallNumbers) {
+            //answersBallNumbers = BallNumbers : 원래 있던 값(정답)
+            //userBallNumbers : 유저가 넣어준 값
+            //userBallNumbers로 만든 Balls 객체 : userBalls
+            BallStatus status = userBalls.play(BallNumbers);
             result.report(status);
         }
 
